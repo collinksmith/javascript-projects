0836497ae0ae633adfa4b11772aa3e57e106e736
Collin Smith <collink33@gmail.com>
2015-07-23 16:42:06 -0700

    Add days and times to courses

--- class-exercises.js
+++ class-exercises.js
@@ -18,11 +18,23 @@ Student.prototype.name = function () {
 }
 
 Student.prototype.enroll = function (course) {
-  if ( this.courses.indexOf(course) < 0) {
+  if (this.has_conflict(course)){
+    console.log("Cannot enroll, there is a clash");
+  } else {
     this.courses.push(course);
-    course.add_student(this);
+    course.students.push(this);
   }
 }
+
+Student.prototype.has_conflict = function (course) {
+  var is_true = false;
+  this.courses.forEach ( function (this_course) {
+    if (this_course.conflictsWith(course)){
+      is_true = true;
+    }
+  })
+  return is_true;
+};
 
 Student.prototype.course_load = function () {
   result = {};
@@ -37,9 +49,22 @@ Student.prototype.course_load = function () {
 }
 
 Course.prototype.add_student = function (student) {
-  if ( this.students.indexOf(student) < 0 ) {
-    this.students.push(student)
-    student.enroll(this)
+  this.students.push(student)
+  student.courses.push(this)
+  }
+
+Course.prototype.conflictsWith = function (course) {
+  if (!(this.time_block === course.time_block)){
+    return false;
+  }else {
+    var days_this = this.days
+    var is_true = false
+    course.days.forEach( function (day) {
+      if (days_this.indexOf(day) > 0){
+        is_true =  true;
+      }
+    })
+    return is_true;
   }
 };
 
@@ -48,9 +73,9 @@ studenta = new Student("Buck", "Shlegeris")
 studentb = new Student("Joe", "Johnson")
 
 coursea = new Course("Gen Chem", "Chemistry", 4, ['mon', 'wed', 'fri'], 2)
-courseb = new Course("Evolution", "Biology", 4, ['tue', 'thurs', 2])
+courseb = new Course("Evolution", "Biology", 4, ['tue', 'thurs'], 2)
 coursec = new Course("Rock Climbing", "PE", 2, ['mon', 'wed'], 2)
-coursed = new Course("O. Chem", "Chemistry", 9, ['mon', 'wed', 'fri', 4])
+coursed = new Course("O. Chem", "Chemistry", 9, ['mon', 'wed', 'fri'], 4)
 
 studenta.enroll(coursea)
 studenta.enroll(courseb)
